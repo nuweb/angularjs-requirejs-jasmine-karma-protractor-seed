3e12c0ab36ddfcd1a08f5200dd97d470c93c7a04
Chandra Sekhar Veera <veeracs@icloud.com>
2015-01-13 15:50:23 -0500

    update refs

---
+++ client/app/js/main.js
@@ -0,0 +1,24 @@
+requirejs.config({
+    paths: {
+        angular: '../bower_components/angular/angular',
+        angularRoute: '../bower_components/angular-route/angular-route'
+    }
+});
+
+/*
+require.config({
+        paths: {
+            angular: '../libs/angular',
+            angularResource: '../libs/angular-resource',
+            jquery: '../components/jquery/jquery.min',
+            bootstrap: '../libs/bootstrap.min',
+            angularStrap: '../components/angular-strap/dist/angular-strap.min',
+            constants: 'common/constants/constants',
+            directives: 'common/directives/directives',
+            services: 'common/services/services',
+            security: 'common/security/security',
+            cn: '../libs/omniture/cn',
+            s_code: '../libs/omniture/s_code',
+            omniture: '../libs/omniture/cnid_omniture'
+        },
+        */
