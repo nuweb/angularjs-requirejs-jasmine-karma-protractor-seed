19dc703d3d387b6f8acc3304060d5f644dc29c62
Chandra Sekhar Veera <veeracs@icloud.com>
2015-01-13 20:37:19 -0500

    add shim

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -4,5 +4,16 @@ requirejs.config({
         angularRoute: '../bower_components/angular-route/angular-route',
         jquery: '../bower_components/jquery/dist/jquery.min',
         bootstrap: '../bower_components/bootstrap/dist/bootstrap.min'
+    },
+    shim: {
+        'angularRoute': {
+            deps: ['angular']
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'app': {
+            deps: ['angular', 'angularRoute', 'bootstrap']
+        }
     }
 });
